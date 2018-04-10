943a40652b76d1bbef9739e229965487a7db114d
Kaysy123 <38130018+Kaysy123@users.noreply.github.com>
2018-04-10 16:21:27 +0100

    Add files via upload

---
+++ hng1/index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<link rel="stylesheet" type="text/css" href="css/stylesheet.css" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>HNG TASK 1</title>
+
+</head>
+
+<body>
+	<header>
+    	<div class="rightwing">
+            <ul>
+                <li><a href="#">Home</a> </li>
+                <li><a href="#">About Us</a></li>
+                <li><a href="#">Contact Us</a></li>
+                <li><a href="#">Bookings</a></li>
+            </ul>
+        </div> 
+
+    </header>
+    <section class="text-container">
+        <div class="mt center">
+            <p> Welcome Guest </p>
+        </div>
+                <p> <em>...neva get stranded</em></p>
+            </div>
+          
+    </section>
+     
+   
+    <section>
+        <div class="righttext">
+            <div id="timer"> 
+                <?php
+                echo date("h:i:s A");
+                ?>
+            </div>
+            <div id="date">
+                <?php echo date("l jS \of F Y")
+                ?>
+            </div> 
+        </div>
+
+        <div id="bottomtext"> <p> hotels.ng internship task</p></div>
+        
+        </div>
+        <div class="lefttext">
+            <div><p> HNG </p></div>
+        </div>
+
+    </section>
+</body>
+</html>
